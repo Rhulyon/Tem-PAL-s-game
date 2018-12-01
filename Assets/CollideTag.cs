aeee2ca8dee8d13a1c73998724e9927a46986b76
Waaru <nicolas.roland9@gmail.com>
2018-12-01 17:47:00 +0100

    test tag

---
+++ Assets/CollideTag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideTag : MonoBehaviour
+{
+    public string LetGo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == LetGo)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+}
