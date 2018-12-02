9a379550a3d91c42e5affd51ac0a6f87bce4a5a4
Rhulyon <rambo11400@gmail.com>
2018-12-02 16:45:31 +0100

    Sound Thingy done

---
+++ Assets/Scripts/Effects/SoundPlayOnceAndLoop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayOnceAndLoop : MonoBehaviour
+{
+    private AudioSource aS;
+
+    public AudioClip introAudio;
+    public AudioClip loopAudio;
+    private void Awake()
+    {
+        aS = this.GetComponent<AudioSource>();
+        aS.clip = introAudio;
+        aS.loop = false;
+    }
+
+    private void Update()
+    {
+        if (aS.isPlaying)
+            return;
+        aS.clip = loopAudio;
+        aS.loop = true;
+        aS.Play();
+        this.enabled = false;
+    }
+}
