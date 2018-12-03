ffc48cee4bfb93bd68f5b6c7477b523d10541f7e
Waaru <nicolas.roland9@gmail.com>
2018-12-03 16:20:51 +0100

    Hole prefab and script add

---
+++ Assets/hole.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class hole : MonoBehaviour
+{
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        IsometricController ic = other.GetComponent<IsometricController>();
+        Destroy(ic.gameObject);
+    }
+}
